181ba52c72989ea9df9928fe4ee4237996415225
antonio.sanchez <antsgon@gmail.com>
2026-02-24 23:32:09 +0100

    Add Procfile and runtime.txt

--- app/config.py
+++ app/config.py
@@ -1,20 +1,14 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from functools import lru_cache
 
-
 class Settings(BaseSettings):
-    """
-    Lee las variables de entorno del archivo .env
-    """
     database_url: str
 
-    class Config:
-        env_file = ".env"
-
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8"
+    )
 
 @lru_cache
 def get_settings():
-    """
-    Devuelve la configuración (cacheada para no leer .env cada vez)
-    """
     return Settings()
